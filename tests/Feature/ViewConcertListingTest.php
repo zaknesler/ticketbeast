2dc492903ffec4be8b461dfbc96874dfb2ddf17e
Zak Nesler <zaknesler@gmail.com>
2019-06-24 09:05:17 -0400

    Add back concert show test

--- tests/Feature/ViewConcertListingTest.php
+++ tests/Feature/ViewConcertListingTest.php
@@ -11,42 +11,42 @@ class ViewConcertListingTest extends TestCase
 {
     use RefreshDatabase;
 
-    // Save for Dusk
-
     /** @test */
-    // function user_can_view_a_published_concert_listing()
-    // {
-    //     $concert = factory(Concert::class)->states('published')->create([
-    //         'title' => 'The Red Chord',
-    //         'subtitle' => 'with Animosity and Lethargy',
-    //         'date' => Carbon::parse('December 13, 2016 8:00pm'),
-    //         'ticket_price' => 3250,
-    //         'venue' => 'The Mosh Pit',
-    //         'venue_address' => '123 Example Ln.',
-    //         'city' => 'Laraville',
-    //         'state' => 'ON',
-    //         'zip' => '90210',
-    //         'additional_information' => 'For support, call (555) 555-5555.',
-    //     ]);
+    function user_can_view_a_published_concert_listing()
+    {
+        $concert = factory(Concert::class)->states('published')->create([
+            'title' => 'The Red Chord',
+            'subtitle' => 'with Animosity and Lethargy',
+            'date' => Carbon::parse('December 13, 2016 8:00pm'),
+            'ticket_price' => 3250,
+            'venue' => 'The Mosh Pit',
+            'venue_address' => '123 Example Ln.',
+            'city' => 'Laraville',
+            'state' => 'ON',
+            'zip' => '90210',
+            'additional_information' => 'For support, call (555) 555-5555.',
+        ]);
 
-    //     $this->get('/concerts/' . $concert->id);
+        $response = $this->get('/concerts/' . $concert->id);
 
-    //     $this->see('The Red Chord');
-    //     $this->see('with Animosity and Lethargy');
-    //     $this->see('December 13, 2016');
-    //     $this->see('8:00pm');
-    //     $this->see('32.50');
-    //     $this->see('The Mosh Pit');
-    //     $this->see('123 Example Ln.');
-    //     $this->see('Laraville, ON 90210');
-    //     $this->see('For support, call (555) 555-5555.');
-    // }
+        $response->assertSee('The Red Chord');
+        $response->assertSee('with Animosity and Lethargy');
+        $response->assertSee('December 13, 2016');
+        $response->assertSee('8:00pm');
+        $response->assertSee('32.50');
+        $response->assertSee('The Mosh Pit');
+        $response->assertSee('123 Example Ln.');
+        $response->assertSee('Laraville, ON 90210');
+        $response->assertSee('For support, call (555) 555-5555.');
+    }
 
     /** @test */
     function user_cannot_view_an_unpublished_concert_listing()
     {
         $concert = factory(Concert::class)->states('unpublished')->create();
 
-        $this->get('/concerts/' . $concert->id)->assertStatus(404);
+        $response = $this->get('/concerts/' . $concert->id);
+
+        $response->assertStatus(404);
     }
 }
