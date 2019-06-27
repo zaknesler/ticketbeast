4523316625b2a321de4b715757c8acfd04d660fe
Zak Nesler <zaknesler@gmail.com>
2019-06-27 13:11:14 -0400

    Add login tests

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Validation\ValidationException;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class LoginController extends Controller
@@ -25,7 +27,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/backstage/concerts';
 
     /**
      * Create a new controller instance.
@@ -35,5 +37,20 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * Get the failed login response instance.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    protected function sendFailedLoginResponse(Request $request)
+    {
+        throw ValidationException::withMessages([
+            $this->username() => [trans('auth.failed')],
+        ])->redirectTo(route('login'));
     }
 }
---
+++ tests/Feature/PromoterLoginTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class PromoterLoginTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    function a_promoter_can_log_in_with_valid_credentials()
+    {
+        $user = factory(User::class)->create([
+            'email' => 'jane@example.com',
+            'password' => Hash::make('secret_password'),
+        ]);
+
+        $response = $this->post('/login', [
+            'email' => 'jane@example.com',
+            'password' => 'secret_password',
+        ]);
+
+        $response->assertRedirect('/backstage/concerts');
+        $this->assertTrue(Auth::check());
+        $this->assertTrue(Auth::user()->is($user));
+    }
+
+    /** @test */
+    function logging_in_with_invalid_credentials()
+    {
+        $user = factory(User::class)->create([
+            'email' => 'jane@example.com',
+            'password' => Hash::make('secret_password'),
+        ]);
+
+        $response = $this->post('/login', [
+            'email' => 'jane@example.com',
+            'password' => 'not-the-right-password',
+        ]);
+
+        $response->assertRedirect('/login');
+        $response->assertSessionHasErrors('email');
+        $this->assertFalse(Auth::check());
+    }
+
+    /** @test */
+    function logging_in_with_an_account_that_does_not_exist()
+    {
+        $response = $this->post('/login', [
+            'email' => 'nobody@example.com',
+            'password' => 'not-the-right-password',
+        ]);
+
+        $response->assertRedirect('/login');
+        $response->assertSessionHasErrors('email');
+        $this->assertFalse(Auth::check());
+    }
+}
