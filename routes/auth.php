9a74c9f85ca78691a021fd3c9d7ba2120032ccf8
Zak Nesler <zaknesler@gmail.com>
2019-06-28 12:07:43 -0400

    Tidy route file

--- routes/auth.php
+++ routes/auth.php
@@ -7,5 +7,7 @@ Route::get('/home', 'HomeController@index')->name('home');
  * Backstage
  * --------------------------------------------------------------------------
  */
-Route::get('/backstage/concerts/new', 'Backstage\\ConcertController@create')->name('backstage.concerts.create');
-Route::post('/backstage/concerts', 'Backstage\\ConcertController@store')->name('backstage.concerts.store');
+Route::prefix('/backstage')->namespace('Backstage')->group(function () {
+    Route::get('/concerts/new', 'ConcertController@create')->name('backstage.concerts.create');
+    Route::post('/concerts', 'ConcertController@store')->name('backstage.concerts.store');
+});
