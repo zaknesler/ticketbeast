3f62c5fe86c515b458a003b19ff035d49ea3bffa
Zak Nesler <zaknesler@gmail.com>
2019-06-29 23:32:34 -0400

    Better seed defaults

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use Carbon\Carbon;
 use App\Models\User;
 use App\Models\Concert;
 use Illuminate\Database\Seeder;
@@ -21,6 +22,33 @@ class DatabaseSeeder extends Seeder
             'password' => Hash::make('password'),
         ]);
 
-        ConcertHelper::createPublished(['user_id' => $user->id, 'ticket_quantity' => 50]);
+        ConcertHelper::createPublished([
+            'user_id' => $user->id,
+            'title' => 'The Red Chord',
+            'subtitle' => 'with Animosity and Lethargy',
+            'additional_information' => 'For support, call (555) 555-5555.',
+            'date' => Carbon::parse('November 25, 2020 8:30pm'),
+            'venue' => 'The Mosh Pit',
+            'venue_address' => '123 Example Lane',
+            'city' => 'Laraville',
+            'state' => 'ON',
+            'zip' => '90210',
+            'ticket_price' => 3250,
+            'ticket_quantity' => 50,
+        ]);
+
+        ConcertHelper::createUnpublished([
+            'user_id' => $user->id,
+            'title' => 'Fat Night',
+            'subtitle' => 'with Ginger Root',
+            'date' => Carbon::parse('December 18, 2021 6:30pm'),
+            'venue' => 'Jazz Hall',
+            'venue_address' => '129 West 81st Street',
+            'city' => 'New York',
+            'state' => 'NY',
+            'zip' => '10024',
+            'ticket_price' => 3250,
+            'ticket_quantity' => 50,
+        ]);
     }
 }
