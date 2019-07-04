5709bdfd7dfa13099156a561e39d075f2492c0b3
Zak Nesler <zaknesler@gmail.com>
2019-07-04 16:48:04 -0400

    Remove database dependency

--- tests/Unit/Mail/AttendeeMessageEmailTest.php
+++ tests/Unit/Mail/AttendeeMessageEmailTest.php
@@ -3,6 +3,8 @@
 namespace Tests\Unit\Mail;
 
 use Tests\TestCase;
+use App\Models\User;
+use App\Models\Concert;
 use App\Models\AttendeeMessage;
 use App\Mail\AttendeeMessageEmail;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -10,12 +12,13 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class AttendeeMessageEmailTest extends TestCase
 {
-    use RefreshDatabase;
-
     /** @test */
     function attendee_message_email_contains_the_subject_and_body()
     {
-        $attendeeMessage = factory(AttendeeMessage::class)->create([
+        $attendeeMessage = factory(AttendeeMessage::class)->make([
+            'concert_id' => factory(Concert::class)->make([
+                'user_id' => factory(User::class)->make(),
+            ]),
             'subject' => 'Test subject',
             'body' => 'Test body',
         ]);
@@ -29,7 +32,10 @@ class AttendeeMessageEmailTest extends TestCase
     /** @test */
     function attendee_message_email_renders_custom_markdown()
     {
-        $attendeeMessage = factory(AttendeeMessage::class)->create([
+        $attendeeMessage = factory(AttendeeMessage::class)->make([
+            'concert_id' => factory(Concert::class)->make([
+                'user_id' => factory(User::class)->make(),
+            ]),
             'subject' => 'Test subject',
             'body' => '# This should be an h1',
         ]);
