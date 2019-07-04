96626bbd1f3e354eb208df915131cdfdc93dc292
Zak Nesler <zaknesler@gmail.com>
2019-07-03 21:21:57 -0400

    Uncomment dusk tests

--- tests/Browser/PromoterLoginTest.php
+++ tests/Browser/PromoterLoginTest.php
@@ -13,37 +13,37 @@ class PromoterLoginTest extends DuskTestCase
     use DatabaseMigrations;
 
     /** @test */
-    // function user_can_log_in_successfully()
-    // {
-    //     $user = factory(User::class)->create([
-    //         'email' => 'john@example.com',
-    //         'password' => Hash::make('secret-password'),
-    //     ]);
+    function user_can_log_in_successfully()
+    {
+        $user = factory(User::class)->create([
+            'email' => 'john@example.com',
+            'password' => Hash::make('secret-password'),
+        ]);
 
-    //     $this->browse(function (Browser $browser) {
-    //         $browser->visitRoute('auth.login')
-    //                 ->type('email', 'john@example.com')
-    //                 ->type('password', 'secret-password')
-    //                 ->press('Sign in')
-    //                 ->assertRouteIs('backstage.concerts.index');
-    //     });
-    // }
+        $this->browse(function (Browser $browser) {
+            $browser->visitRoute('auth.login')
+                    ->type('email', 'john@example.com')
+                    ->type('password', 'secret-password')
+                    ->press('Sign in')
+                    ->assertRouteIs('backstage.concerts.index');
+        });
+    }
 
     /** @test */
-    // function user_cannot_log_in_with_invalid_credentials()
-    // {
-    //     $user = factory(User::class)->create([
-    //         'email' => 'jane@example.com',
-    //         'password' => Hash::make('secret-password'),
-    //     ]);
+    function user_cannot_log_in_with_invalid_credentials()
+    {
+        $user = factory(User::class)->create([
+            'email' => 'jane@example.com',
+            'password' => Hash::make('secret-password'),
+        ]);
 
-    //     $this->browse(function (Browser $browser) {
-    //         $browser->visitRoute('auth.login')
-    //                 ->type('email', 'jane@example.com')
-    //                 ->type('password', 'wrong-password')
-    //                 ->press('Sign in')
-    //                 ->assertRouteIs('auth.login')
-    //                 ->assertSee('credentials do not match');
-    //     });
-    // }
+        $this->browse(function (Browser $browser) {
+            $browser->visitRoute('auth.login')
+                    ->type('email', 'jane@example.com')
+                    ->type('password', 'wrong-password')
+                    ->press('Sign in')
+                    ->assertRouteIs('auth.login')
+                    ->assertSee('credentials do not match');
+        });
+    }
 }
